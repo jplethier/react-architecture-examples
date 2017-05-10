f475e4e8c37b07893c81797a68722461e44275ea
João Paulo Lethier <jplethier@gmail.com>
2017-05-10 20:31:23 -0300

    implements movies list;

--- src/containers/MoviesListPage.js
+++ src/containers/MoviesListPage.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 import './Movies.css';
+import { search } from '../actions/search';
 import Search from '../components/Search';
 import List from '../components/List';
 
 class MoviesListPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+
+    _.bindAll(this, 'onChange');
+  }
+
+  onChange(e) {
+    search(e.target.value).then((movies) => {
+      this.setState({
+        movies: movies
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-        <Search />
-        <List />
+        <Search onChange={this.onChange}/>
+        <List movies={this.state.movies} />
       </div>
     );
   }
