b38db40d152170857f729580f9ca17bf4655fe7d
João Paulo Lethier <jplethier@gmail.com>
2017-05-09 22:57:36 -0300

    adds react router;

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+import App from './containers/App';
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  (<Router>
+    <div>
+      <App/>
+      <Route exact path="/" component={App}/>
+      <Route path="/about" component={App}/>
+      <Route path="/topics" component={App}/>
+    </div>
+  </Router>),
   document.getElementById('root')
 );
