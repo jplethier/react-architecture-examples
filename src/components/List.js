03dc43c6641fd1b8a63755721d7aa04445c992cb
João Paulo Lethier <jplethier@gmail.com>
2017-05-10 00:13:30 -0300

    rename containers and add moviepage;

---
+++ src/components/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './List.css';
+
+class List extends Component {
+  render() {
+    return (
+      <div className="List">
+        Listagem de filmes
+      </div>
+    );
+  }
+}
+
+export default List;
